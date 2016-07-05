59c9a7f769f2ae880fe75b77253c1ef739f085f2
Parthu K <krisparthu@gmail.com>
2016-07-05 01:47:36 -0700

    allin one
    installs the app, generates and stores the key pair and setups the key pair on the chef compliance server

--- scripts/p2porchAllinOneCC.sh
+++ scripts/p2porchAllinOneCC.sh
@@ -5,6 +5,21 @@
 # 2. Generate a KeyPair
 # 3. Bootstrap the Private Key to the Chef Compliance Server
 
+keyName=$1
+componentAdminUsername=$2
+componentAdminPassword=$3
+componentBaseUrl=$4
+orchestratorbaseurl=$5
+email=$6
+component=$7
 
 
+# Step 1
+sh p2pinstallOrchestratorApp.sh
+
+# Step 2
+sh p2porchestratorgenKeyPair.sh keyName componentAdminUsername componentAdminPassword componentBaseUrl orchestratorbaseurl email component
+
+# Step3 
+sh p2porchestratorBootstrapChefCompliance.sh keyName componentAdminUsername componentAdminPassword componentBaseUrl orchestratorbaseurl email "chefcc"
 

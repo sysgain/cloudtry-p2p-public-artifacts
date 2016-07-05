c1da044f2b0dbd560eaa7b8ab411e13fdf5f3b71
Parthu K <krisparthu@gmail.com>
2016-07-05 01:09:30 -0700

    created the script to call API call
    this is the basic script to check if it runs. If it works well, add better error handling and response validation to the script.Decent source here for a sample:
    https://github.com/Azure/azure-quickstart-templates/blob/875d139c16c9c023dce519e6dd48c707e3473346/shared_scripts/ubuntu/afs-utils-0.1.sh

--- scripts/p2porchestratorCCSetup.sh
+++ scripts/p2porchestratorCCSetup.sh
@@ -1,1 +1,12 @@
+#!/bin/bash
+keyName=$1
+password=$2
+email=$3
+componentadminusername=$4
+component=$5
+orchestratorbaseurl=$6
 
+URL="http://$orchestratorbaseurl:33001/keys/$component/$componentadminusername/keypairs"
+echo $URL
+# this api generates a key pair and stores it in the database for a particular component admin user. The email is used for creating public private key
+curl -H "Content-Type: application/json" -X POST -d '{"keyName":"'"$keyName"'", "password":"'"$password"'", "email":"'"$email"'"}' $URL

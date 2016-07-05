3871663147835f57e1a7d7a9642597cc670694d8
Parthu K <krisparthu@gmail.com>
2016-07-05 01:22:30 -0700

    created the base script.
    Needs to be updated for error handling

--- scripts/p2porchestratorBootstrapChefCompliance.sh
+++ scripts/p2porchestratorBootstrapChefCompliance.sh
@@ -1,1 +1,12 @@
+#!/bin/bash
+keyName=$1
+componentAdminUsername=$2
+componentAdminPassword=$3
+componentBaseUrl=$4
+orchestratorbaseurl=$5
+component="chefcc"
 
+URL="http://$orchestratorbaseurl:33001/keys/$component/$componentAdminUsername/component"
+echo $URL
+# this api setups the private key  on the component - In this case, uploads the PRIVATE key to chef compliance. Response if successful is 200ok
+curl -H "Content-Type: application/json" -X POST -d '{"keyName":"'"$keyName"'", "ownerUserName":"'"$componentAdminUsername"'","ownerPassword":"'"$componentAdminPassword"'", "componentBaseUrl":"'"$componentBaseUrl"'"}' $URL

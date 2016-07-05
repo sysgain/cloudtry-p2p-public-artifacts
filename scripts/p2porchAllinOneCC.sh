0a675cb4263992d36fbc70b96b73d3550a561be7
Parthu K <krisparthu@gmail.com>
2016-07-05 01:40:18 -0700

    base file, needs exceptions handling

---
+++ scripts/p2porchAllinOneCC.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+# this will perform the following tasks
+# 1. Install the Orchestrator Application in docker
+# 2. Generate a KeyPair
+# 3. Bootstrap the Private Key to the Chef Compliance Server
+
+
+
+

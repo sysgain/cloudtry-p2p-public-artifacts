d20dd84f024a7e6da0a82f666818e227faa47300
Parthu K <krisparthu@gmail.com>
2016-07-05 01:43:14 -0700

    made it similar to bootstrapcc script

--- scripts/p2porchestratorgenKeyPair.sh
+++ scripts/p2porchestratorgenKeyPair.sh
@@ -1,12 +1,13 @@
 #!/bin/bash
 keyName=$1
-password=$2
-email=$3
-componentadminusername=$4
-component=$5
-orchestratorbaseurl=$6
+componentAdminUsername=$2
+componentAdminPassword=$3
+componentBaseUrl=$4
+orchestratorbaseurl=$5
+email=$6
+component=$7
 
-URL="http://$orchestratorbaseurl:33001/keys/$component/$componentadminusername/keypairs"
+URL="http://$orchestratorbaseurl:33001/keys/$component/$componentAdminUsername/keypairs"
 echo $URL
 # this api generates a key pair and stores it in the database for a particular component admin user. The email is used for creating public private key
-curl -H "Content-Type: application/json" -X POST -d '{"keyName":"'"$keyName"'", "password":"'"$password"'", "email":"'"$email"'"}' $URL
+curl -H "Content-Type: application/json" -X POST -d '{"keyName":"'"$keyName"'", "password":"'"$componentAdminPassword"'", "email":"'"$email"'"}' $URL

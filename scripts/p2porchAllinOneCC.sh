f3e32c1bd1d1cdefd5aa01cda9a9d62a9127d1e4
Parthu K <krisparthu@gmail.com>
2016-07-05 20:28:55 -0700

    Update p2porchAllinOneCC.sh

--- scripts/p2porchAllinOneCC.sh
+++ scripts/p2porchAllinOneCC.sh
@@ -13,14 +13,18 @@ orchestratorbaseurl=$5
 email=$6
 component=$7
 
+echo $componentAdminUsername
+
 git clone https://github.com/sysgain/km-docker-nodejs.git ~/km-docker-nodejs2
 cd ~/km-docker-nodejs2
 COMPOSE_HTTP_TIMEOUT=180 sudo docker-compose up -d
 sleep 120
 COMPOSE_HTTP_TIMEOUT=240 sudo docker-compose up -d
 sleep 120
+echo $componentAdminUsername
 URL="http://$orchestratorbaseurl:33001/keys/$component/$componentAdminUsername/keypairs"
 echo $URL
+sleep 30
 # this api generates a key pair and stores it in the database for a particular component admin user. The email is used for creating public private key
 curl -H "Content-Type: application/json" -X POST -d '{"keyName":"'"$keyName"'", "password":"'"$componentAdminPassword"'", "email":"'"$email"'"}' $URL
 

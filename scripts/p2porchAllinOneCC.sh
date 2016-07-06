914f9b7b8cd125d20ca963ba7cb8e4c873951324
Parthu K <krisparthu@gmail.com>
2016-07-05 19:30:21 -0700

    Update p2porchAllinOneCC.sh

--- scripts/p2porchAllinOneCC.sh
+++ scripts/p2porchAllinOneCC.sh
@@ -16,7 +16,8 @@ component=$7
 git clone https://github.com/sysgain/km-docker-nodejs.git ~/km-docker-nodejs2
 cd ~/km-docker-nodejs2
 COMPOSE_HTTP_TIMEOUT=180 sudo docker-compose up -d
-sudo docker-compose up -d
+sleep 120
+COMPOSE_HTTP_TIMEOUT=240 sudo docker-compose up -d
 sleep 120
 URL="http://$orchestratorbaseurl:33001/keys/$component/$componentAdminUsername/keypairs"
 echo $URL

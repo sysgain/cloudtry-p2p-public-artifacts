88b58b57bad17212b1988a81e90cae1434b7aa78
Parthu K <krisparthu@gmail.com>
2016-07-05 18:20:53 -0700

    added back all commands into one script

--- scripts/p2porchAllinOneCC.sh
+++ scripts/p2porchAllinOneCC.sh
@@ -13,13 +13,17 @@ orchestratorbaseurl=$5
 email=$6
 component=$7
 
+git clone https://github.com/sysgain/km-docker-nodejs.git ~/km-docker-nodejs2
+cd ~/km-docker-nodejs2
+COMPOSE_HTTP_TIMEOUT=180 sudo docker-compose up -d
+sudo docker-compose up -d
 
-# Step 1
-sh p2pinstallOrchestratorApp.sh
+URL="http://$orchestratorbaseurl:33001/keys/$component/$componentAdminUsername/keypairs"
+echo $URL
+# this api generates a key pair and stores it in the database for a particular component admin user. The email is used for creating public private key
+curl -H "Content-Type: application/json" -X POST -d '{"keyName":"'"$keyName"'", "password":"'"$componentAdminPassword"'", "email":"'"$email"'"}' $URL
 
-# Step 2
-sh p2porchestratorgenKeyPair.sh keyName componentAdminUsername componentAdminPassword componentBaseUrl orchestratorbaseurl component email
-
-# Step3 
-sh p2porchestratorBootstrapChefCompliance.sh keyName componentAdminUsername componentAdminPassword componentBaseUrl orchestratorbaseurl "chefcc" email
-
+URL="http://$orchestratorbaseurl:33001/keys/$component/$componentAdminUsername/component"
+echo $URL
+# this api setups the private key  on the component - In this case, uploads the PRIVATE key to chef compliance. Response if successful is 200ok
+curl -H "Content-Type: application/json" -X POST -d '{"keyName":"'"$keyName"'", "ownerUserName":"'"$componentAdminUsername"'","ownerPassword":"'"$componentAdminPassword"'", "componentBaseUrl":"'"$componentBaseUrl"'"}' $URL

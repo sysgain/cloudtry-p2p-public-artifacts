0a3c3088b0ac3569778752dd08801d210bb797d7
Parthu K <krisparthu@gmail.com>
2016-07-05 18:43:46 -0700

    added sleep timer so that service gets time to boot up

--- scripts/p2porchAllinOneCC.sh
+++ scripts/p2porchAllinOneCC.sh
@@ -17,7 +17,7 @@ git clone https://github.com/sysgain/km-docker-nodejs.git ~/km-docker-nodejs2
 cd ~/km-docker-nodejs2
 COMPOSE_HTTP_TIMEOUT=180 sudo docker-compose up -d
 sudo docker-compose up -d
-
+sleep 120
 URL="http://$orchestratorbaseurl:33001/keys/$component/$componentAdminUsername/keypairs"
 echo $URL
 # this api generates a key pair and stores it in the database for a particular component admin user. The email is used for creating public private key

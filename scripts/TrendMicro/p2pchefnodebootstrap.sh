2fe5d00e2f88d7e4cc6b429312b5401b559ccca9
Parthu K <krisparthu@gmail.com>
2016-07-07 14:16:29 -0700

    created the first script for chef node extension

---
+++ scripts/TrendMicro/p2pchefnodebootstrap.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+keyName=$1
+orchestratorbaseurl=$2
+
+URL="http://$orchestratorbaseurl:33001/key/$keyName"
+echo $URL
+
+
+#this API retrieves the validator.pem given a keyname, decodes it and stores it in validator.pem file
+sudo curl -H "Content-Type: application/json" -X GET $URL | base64 --decode > /etc/chef/validator.pem
+
+
+#Next step is run sudo chef-client so that it boot straps to server
+sudo chef-client
+

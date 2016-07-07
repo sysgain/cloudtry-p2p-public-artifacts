d48f46a3501d73a9e4d64981d37a410d629dacaf
Parthu K <krisparthu@gmail.com>
2016-07-07 15:06:04 -0700

    added tr to remove quotes

--- scripts/TrendMicro/p2pchefnodebootstrap.sh
+++ scripts/TrendMicro/p2pchefnodebootstrap.sh
@@ -7,7 +7,7 @@ echo $URL
 
 
 #this API retrieves the validator.pem given a keyname, decodes it and stores it in validator.pem file
-sudo curl -H "Content-Type: application/json" -X GET $URL | base64 --decode > /etc/chef/validation.pem
+sudo curl -H "Content-Type: application/json" -X GET $URL | tr -d "\"" | base64 --decode > /etc/chef/validation.pem
 
 
 #Next step is run sudo chef-client so that it boot straps to server

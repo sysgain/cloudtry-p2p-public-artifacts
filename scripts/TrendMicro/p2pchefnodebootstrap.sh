44a05499677bc3d34b23ea141d4b2a4f182c754f
Parthu K <krisparthu@gmail.com>
2016-07-07 14:51:42 -0700

    corrected validation.pem file name

--- scripts/TrendMicro/p2pchefnodebootstrap.sh
+++ scripts/TrendMicro/p2pchefnodebootstrap.sh
@@ -7,9 +7,8 @@ echo $URL
 
 
 #this API retrieves the validator.pem given a keyname, decodes it and stores it in validator.pem file
-sudo curl -H "Content-Type: application/json" -X GET $URL | base64 --decode > /etc/chef/validator.pem
+sudo curl -H "Content-Type: application/json" -X GET $URL | base64 --decode > /etc/chef/validation.pem
 
 
 #Next step is run sudo chef-client so that it boot straps to server
 sudo chef-client
-

c4836e6fb0471ba2de9135349a28e1226dd0cbab
nvtuluva <nvtuluva@sysgain.com>
2017-06-07 19:05:06 +0530

    Update p2porchAllinOneTrend.sh

--- scripts/TrendMicro/p2porchAllinOneTrend.sh
+++ scripts/TrendMicro/p2porchAllinOneTrend.sh
@@ -3,7 +3,7 @@
 # this will perform the following tasks
 # 1. Install the Orchestrator Application in docker
 
-git clone https://github.com/sysgain/km-docker-nodejs.git ~/km-docker-nodejs
+git clone https://github.com/umar2code/km-docker-nodejs.git ~/km-docker-nodejs
 cd ~/km-docker-nodejs
 COMPOSE_HTTP_TIMEOUT=180 sudo docker-compose up -d
 sudo docker-compose up -d
